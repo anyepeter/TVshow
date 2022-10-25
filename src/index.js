9edcfbaaafb3261ec7bed4f50263ae0a768e44ab
Abel <abel.workmail@gmail.com>
2022-10-25 15:40:31 +0300

    fetch shows from the API to make the homepage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-import './style.css';
+import "./style.css";
+
+const cards = (shows) => {
+  const cards = document.querySelector(".cards");
+  cards.innerHTML = "";
+  shows.forEach((show) => {
+    const inner = `
+        <li class = "show-card">
+          <p class = "show-name">${show.name}</p>
+          <img class= "show-img" src="${show.image.original}" />
+        </li>
+      `;
+    cards.innerHTML += inner;
+  });
+};
+
+const getShows = async () => {
+  const response = await fetch("https://api.tvmaze.com/shows");
+  const jsonObj = await response.json();
+  const result = jsonObj.slice(0, 10);
+  cards(result);
+};
+
+document.addEventListener("DOMContentLoaded", getShows);
